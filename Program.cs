aab4ba85258898dc6622679845137c81f18f5f04
Detective-Coder <aaronfrankenfield@gmail.com>
2021-04-23 15:38:19 -0400

    phase two

--- Program.cs
+++ Program.cs
@@ -1,35 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace heist
 {
     class Program
     {
-        public class singleTeamMember
-        {
-          public string Name {get; set;}
-          public int SkillLevel {get; set;}
-          public double CourageFactor {get; set;}
-        }
+    
         static void Main(string[] args)
         {
-            Console.WriteLine("Plan Your Heist!");
+            // // phase one
+            // Console.WriteLine("Plan Your Heist!");          
+      
+            // Console.WriteLine("What is the team member's name?");
+            // string CriminalName = Console.ReadLine();
 
-            // Prompt the user to enter a team member's name and save that name.
-            Console.WriteLine("Please enter the team member's name:");
-            string Name = Console.ReadLine();
+            // Console.WriteLine("What is the team member's skill level?");
+            // int SkillLevel = Convert.ToInt32(Console.ReadLine());
 
-            // Prompt the user to enter a team member's skill level and save that skill level with the name.
-            Console.WriteLine("Please enter the team member's skill level:");
-            string SkillLevel = Console.ReadLine();
+            // Console.WriteLine("What is the team member's courage?");
+            // string Courage = Console.ReadLine();
 
-            // Prompt the user to enter a team member's courage factor and save that courage factor with the name.
-            Console.WriteLine("Please enter the team member's courage factor:");
-            string CourageFactor = Console.ReadLine();
+            // Console.WriteLine();
+            // Console.WriteLine("Team Member One:");
+            // Console.WriteLine($"Name: {CriminalName}, skill level: {SkillLevel}, Courage: {Courage}");
 
-            Console.WriteLine();
-            Console.WriteLine("Team Member One:");
-            Console.WriteLine($"{Name}, {SkillLevel}, {CourageFactor}");
+            // phase 2
+            List<SingleTeamMember> criminalList = new List<SingleTeamMember>();
 
-        }
+            while(true)
+            {
+              Console.WriteLine("What is the team member's name?");
+              string CriminalName;
+              CriminalName = Console.ReadLine();
+              if(CriminalName == "")
+              {
+                break;
+              }
+              Console.WriteLine("What is the team member's skill level?");
+              int SkillLevel;
+              SkillLevel = Convert.ToInt32(Console.ReadLine());
+
+              Console.WriteLine("What is the team member's courage?");
+              double CourageFactor;
+              CourageFactor = Convert.ToDouble(Console.ReadLine());
+
+              Console.WriteLine();
+              Console.WriteLine($"Name: {CriminalName}, Skill Level: {SkillLevel}, Courage: {CourageFactor}");
+
+              SingleTeamMember criminal = new SingleTeamMember();
+              criminal.Name = CriminalName;
+              criminal.SkillLevel = SkillLevel;
+              criminal.CourageFactor = CourageFactor;
+              
+              criminalList.Add(criminal);
+            }
+
+        } 
     }
 }
